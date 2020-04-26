fd11530dafbc409640436b4427d5381e3382b968
teh6 <teh6@st-andrews.ac.uk>
2020-04-26 22:28:10 +0100

    Scripts that start the benchmarks in various configs.

---
+++ gem5-commands/configs/ff-barnes-init.sh
@@ -0,0 +1,12 @@
+for P in 1 2 4 8 16
+do
+    ../benchmarks/ff-barnes.sh $1 4 2 $P fast-forward &
+    ../benchmarks/ff-barnes.sh $1 1 1 $P fast-forward &
+    ../benchmarks/ff-barnes.sh $1 2 2 $P fast-forward &
+    ../benchmarks/ff-barnes.sh $1 3 3 $P fast-forward &
+    ../benchmarks/ff-barnes.sh $1 4 4 $P fast-forward &
+    ../benchmarks/ff-barnes.sh $1 2 4 $P fast-forward &
+    ../benchmarks/ff-barnes.sh $1 1 3 $P fast-forward &
+    ../benchmarks/ff-barnes.sh $1 3 1 $P fast-forward
+done
+
