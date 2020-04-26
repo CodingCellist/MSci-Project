ea48d2f46da297d297f356f79ec091d0718940cc
teh6 <teh6@st-andrews.ac.uk>
2020-04-26 22:50:23 +0100

    Things should timeout.

--- gem5-commands/configs/ff-water-spatial-init.sh
+++ gem5-commands/configs/ff-water-spatial-init.sh
@@ -1,12 +1,13 @@
+#! /usr/bin/env bash
 for P in 1 2 4 8 16
 do
-    ../benchmarks/ff-water-spatial.sh $1 4 2 $P fast-forward &
-    ../benchmarks/ff-water-spatial.sh $1 1 1 $P fast-forward &
-    ../benchmarks/ff-water-spatial.sh $1 2 2 $P fast-forward &
-    ../benchmarks/ff-water-spatial.sh $1 3 3 $P fast-forward &
-    ../benchmarks/ff-water-spatial.sh $1 4 4 $P fast-forward &
-    ../benchmarks/ff-water-spatial.sh $1 2 4 $P fast-forward &
-    ../benchmarks/ff-water-spatial.sh $1 1 3 $P fast-forward &
-    ../benchmarks/ff-water-spatial.sh $1 3 1 $P fast-forward
+    timeout 2h ../benchmarks/ff-water-spatial.sh $1 4 2 $P fast-forward &
+    timeout 2h ../benchmarks/ff-water-spatial.sh $1 1 1 $P fast-forward &
+    timeout 2h ../benchmarks/ff-water-spatial.sh $1 2 2 $P fast-forward &
+    timeout 2h ../benchmarks/ff-water-spatial.sh $1 3 3 $P fast-forward &
+    timeout 2h ../benchmarks/ff-water-spatial.sh $1 4 4 $P fast-forward &
+    timeout 2h ../benchmarks/ff-water-spatial.sh $1 2 4 $P fast-forward &
+    timeout 2h ../benchmarks/ff-water-spatial.sh $1 1 3 $P fast-forward &
+    timeout 2h ../benchmarks/ff-water-spatial.sh $1 3 1 $P fast-forward
 done
 
--- gem5-commands/configs/ff-water-spatial-only-bL.sh
+++ gem5-commands/configs/ff-water-spatial-only-bL.sh
@@ -1,13 +1,13 @@
 #! /usr/bin/env bash
 for P in 1 2 4 8 16
 do
-    ../benchmarks/ff-water-spatial.sh $1 0 1 $P fast-forward &
-    ../benchmarks/ff-water-spatial.sh $1 0 2 $P fast-forward &
-    ../benchmarks/ff-water-spatial.sh $1 0 3 $P fast-forward &
-    ../benchmarks/ff-water-spatial.sh $1 0 4 $P fast-forward &
-    ../benchmarks/ff-water-spatial.sh $1 1 0 $P fast-forward &
-    ../benchmarks/ff-water-spatial.sh $1 2 0 $P fast-forward &
-    ../benchmarks/ff-water-spatial.sh $1 3 0 $P fast-forward &
-    ../benchmarks/ff-water-spatial.sh $1 4 0 $P fast-forward
+    timeout 2h ../benchmarks/ff-water-spatial.sh $1 0 1 $P fast-forward &
+    timeout 2h ../benchmarks/ff-water-spatial.sh $1 0 2 $P fast-forward &
+    timeout 2h ../benchmarks/ff-water-spatial.sh $1 0 3 $P fast-forward &
+    timeout 2h ../benchmarks/ff-water-spatial.sh $1 0 4 $P fast-forward &
+    timeout 2h ../benchmarks/ff-water-spatial.sh $1 1 0 $P fast-forward &
+    timeout 2h ../benchmarks/ff-water-spatial.sh $1 2 0 $P fast-forward &
+    timeout 2h ../benchmarks/ff-water-spatial.sh $1 3 0 $P fast-forward &
+    timeout 2h ../benchmarks/ff-water-spatial.sh $1 4 0 $P fast-forward
 done
 
