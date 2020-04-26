f6551e97d631661e524876ae1a1761baec86b9ec
teh6 <teh6@st-andrews.ac.uk>
2020-04-26 23:57:19 +0100

    Works now?

--- gem5-commands/configs/ff-fmm-init.sh
+++ gem5-commands/configs/ff-fmm-init.sh
@@ -1,6 +1,7 @@
 #! /usr/bin/env bash
 for P in 1 2 4 8 16
 do
+    echo "############"
     timeout 2h ../benchmarks/ff-fmm.sh $1 4 2 $P fast-forward &
     timeout 2h ../benchmarks/ff-fmm.sh $1 1 1 $P fast-forward &
     timeout 2h ../benchmarks/ff-fmm.sh $1 2 2 $P fast-forward &
@@ -9,9 +10,6 @@ do
     timeout 2h ../benchmarks/ff-fmm.sh $1 2 4 $P fast-forward &
     timeout 2h ../benchmarks/ff-fmm.sh $1 1 3 $P fast-forward &
     timeout 2h ../benchmarks/ff-fmm.sh $1 3 1 $P fast-forward &
-    for J in $(jobs -p)
-    do
-        wait $J
-    done
+    wait
 done
 
