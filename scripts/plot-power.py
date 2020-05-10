0f3c1a81c5fd1c9c430523b92d972a9a0428c8cb
teh6 <teh6@st-andrews.ac.uk>
2020-05-10 15:25:24 +0100

    Plots things on the same graph.

--- scripts/plot-power.py
+++ scripts/plot-power.py
@@ -9,24 +9,35 @@ import math
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('csv_file', type=str, default='roi-out.csv',
-                        help='CSV file with the power data')
+                        help='CSV file with the power data.')
     parser.add_argument('--figsize', type=tuple, default=(10.8, 19.2),
                         help='The size, in 100 pixels(?), of the figures.'
                              ' DEFAULT (10.8, 19.2), i.e. 1080x1920')
     parser.add_argument('--output-dir', type=str, default='plots',
-                        help='Top-level directory to save the plots in')
+                        help='Top-level directory to save the plots in.')
     parser.add_argument('--share-x', action='store_true',
                         help="Don't share the x-axis across subplots."
                              " DEFAULT FALSE")
     parser.add_argument('--no-share-y', action='store_true',
                         help="Don't share the y axis between subplots."
                              " DEFAULT FALSE")
+    parser.add_argument('--split-bl', action='store_true',
+                        help='Save to distinct plots for big and LITTLE cpus.'
+                             ' DEFAULT FALSE, i.e. plot both big+LITTLE on the'
+                             ' same figure.')
     return parser.parse_args()
 
 
-def plot(csv_file: str, outdir: pathlib.Path, figsize: tuple,
+def plot(csv_file: str, outdir: pathlib.Path = pathlib.Path('./plots/'),
+         split_bl: bool = False,
+         figsize: tuple = (10.8, 19.2),
          sharex: bool = False, sharey: bool = True):
     df0 = pd.read_csv(csv_file)
+    # fix the fact that sim_seconds are slightly broken by floats in
+    # data-aggregate.py
+    # FixMe: df0['sim_seconds'] = df0['sim_seconds'].apply(lambda s: round(s, 3))
+
+    # create a copy of df0 that we can add things to
     power_df = df0.loc[:, ('benchmark', 'threads', 'sim_seconds', 'cluster',
                            'cpu', 'dynamic_power', 'static_power')]
     bcs = df0['big_cpus']
@@ -48,29 +59,75 @@ def plot(csv_file: str, outdir: pathlib.Path, figsize: tuple,
                     .dropna(axis=1)
                 # for power_type, cluster, cpu in pdf_mi_unst.axes[1]:
                 for power_type in ['dynamic_power', 'static_power']:
-                    for cluster in ['bigCluster', 'littleCluster']:
-                        # TODO: do big+little on same plot
-                        # conf = 'XbYL' where X, Y = no. big_cs, no. little_cs
-                        c_range = range(int(conf[0])) \
-                                  if cluster == 'bigCluster' \
-                                  else range(int(conf[2]))
-                        output_dir_str = str(output_dir) \
-                                         if output_dir.name.endswith('/') \
-                                         else str(output_dir) + '/'
-                        outfile = output_dir_str \
-                                  + '-'.join([power_type, cluster]) \
-                                  + '.png'
-                        suptitle = ' '.join([cluster] + power_type.split('_'))
-                        print('Making file:', outfile)
-                        _plot_helper(outfile, pdf_mi_unst,
-                                     figsize, sharex, sharey, suptitle,
-                                     power_type, cluster, c_range)
+                    output_dir_str = str(output_dir) \
+                        if output_dir.name.endswith('/') \
+                        else str(output_dir) + '/'
+                    suptitle = ' '.join([bm, conf, 'p' + str(nt)])
+
+                    if not split_bl:
+                        outfile = output_dir_str + power_type + '.png'
+                        suptitle += ' ' + ' '.join(power_type.split('_'))
+                        print('Making joint plot:', outfile)
+                        # conf = 'XbYL'
+                        # where X, Y = no. big_cs, no. little_cs
+                        bc_range = range(int(conf[0]))
+                        lc_range = range(int(conf[2]))
+                        _plot_joint(outfile, pdf_mi_unst, figsize,
+                                    sharex, sharey, suptitle,
+                                    power_type, bc_range, lc_range)
+                    else:
+                        for cluster in ['bigCluster', 'littleCluster']:
+                            # conf = 'XbYL'
+                            # where X, Y = no. big_cs, no. little_cs
+                            c_range = range(int(conf[0])) \
+                                      if cluster == 'bigCluster' \
+                                      else range(int(conf[2]))
+                            outfile = output_dir_str \
+                                      + '-'.join([power_type, cluster]) \
+                                      + '.png'
+                            suptitle += ' '.join([cluster]
+                                                + power_type.split('_'))
+                            print('Making split plot:', outfile)
+                            _plot_split(outfile, pdf_mi_unst,
+                                        figsize, sharex, sharey, suptitle,
+                                        power_type, cluster, c_range)
 
 
-def _plot_helper(outdest, unstacked,
+def _plot_joint(outdest, unstacked,
+                figsize, sharex, sharey, suptitle,
+                power_type, bc_range, lc_range):
+    ncols = 2
+    total_cpus = len(bc_range) + len(lc_range)
+    fig, axes = plt.subplots(nrows=round(total_cpus / 2),
+                             ncols=ncols,
+                             figsize=figsize,
+                             sharex=sharex,
+                             sharey=sharey,
+                             constrained_layout=True
+                             )
+    cpus = list(bc_range) + list(lc_range)
+    for i, ax in zip(range(len(cpus)), axes.flatten()):
+        if i < len(bc_range):
+            cluster = 'bigCluster'
+            cpu_class = 'big'
+            color = 'tab:red'
+        else:
+            cluster = 'littleCluster'
+            cpu_class = 'little'
+            color = 'tab:blue'
+        (unstacked.loc[:, (power_type, cluster, cpus[i])])\
+            .plot(title='-'.join([cpu_class, 'cpu', str(cpus[i])]),
+                  ax=ax,
+                  color=color)
+        ax.legend()
+    fig.suptitle(suptitle, fontsize=14)
+    fig.savefig(outdest)
+    plt.close(fig)
+
+
+def _plot_split(outdest, unstacked,
                  figsize, sharex, sharey, suptitle,
                  power_type, cluster, c_range):
-    # TODO: do big+little on same plot
     n_cores = len(c_range)
     if n_cores > 1:
         fig, axes = plt.subplots(nrows=math.ceil(n_cores / 2),
@@ -90,7 +147,8 @@ def _plot_helper(outdest, unstacked,
                                figsize=figsize,
                                sharex=sharex,
                                sharey=sharey,
-                               constrained_layout=True)
+                               constrained_layout=True
+                               )
         cpu = c_range[0]
         (unstacked.loc[:, (power_type, cluster, cpu)])\
             .plot(title='cpu' + str(cpu),
@@ -107,4 +165,11 @@ outdir.mkdir(parents=True, exist_ok=True)
 sharex = args.share_x
 sharey = not args.no_share_y
 sns.set_style('whitegrid')
-plot(args.csv_file, outdir, args.figsize, sharex, sharey)
+
+plot(csv_file=args.csv_file,
+     outdir=outdir,
+     split_bl=args.split_bl,
+     figsize=args.figsize,
+     sharex=sharex,
+     sharey=sharey
+     )
